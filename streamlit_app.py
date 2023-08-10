458ffe17eb05cabf47da4255fa1a5e48fdff42b6
aghosha <137361488+aghosha@users.noreply.github.com>
2023-08-10 13:47:04 -0400

    Update streamlit_app.py
    added panda library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 streamlit.title('My first Streamlit Code')
 
 streamlit.header('Root1')
@@ -9,3 +11,6 @@ streamlit.header('Root2')
 streamlit.text('node1')
 streamlit.text('node2')
 streamlit.text('node3')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
