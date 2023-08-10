891fc17c25752022404a315944629280cf27f2cf
aghosha <137361488+aghosha@users.noreply.github.com>
2023-08-10 18:07:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,9 @@ streamlit.header('Root2')
 streamlit.text('node1')
 streamlit.text('node2')
 streamlit.text('node3')
+streamlit.text('node4')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc(fruits_selected)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
