b009e0b2dd63632973582e741edbfb574fb30559
aghosha <137361488+aghosha@users.noreply.github.com>
2023-08-10 13:23:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
 streamlit.title('My first Streamlit Code')
 
-streamlit.header('Root')
+streamlit.header('Root1')
 streamlit.text('node1')
 streamlit.text('node2')
+
+streamlit.header('Root2')
+streamlit.text('node1')
+streamlit.text('node2')
+treamlit.text('node3')
